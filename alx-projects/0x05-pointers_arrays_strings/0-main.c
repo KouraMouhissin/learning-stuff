d36492382bfc6402d24f31ff3a05edd5bc350e1d
KouraMouhissin <mouhisin@gmail.com>
2023-05-26 21:22:06 +0000

    comment modifieded

--- alx-projects/0x05-pointers_arrays_strings/0-main.c
+++ alx-projects/0x05-pointers_arrays_strings/0-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
+ * main - Replacing 402 to 98
  *
  * Return: Always 0.
  */
@@ -12,7 +12,9 @@ int main(void)
 
     n = 402;
     printf("n=%d\n", n);
+
     reset_to_98(&n);
+
     printf("n=%d\n", n);
     return (0);
 }
